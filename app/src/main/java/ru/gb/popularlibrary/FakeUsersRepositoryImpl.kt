784b77f5e9de0e3f5aa88b5899dde43340a6b960
vikzak <git@azbukait.ru>
2022-06-26 12:53:11 +0300

    добавлена имплементация recylerView для пользователей

---
+++ app/src/main/java/ru/gb/popularlibrary/FakeUsersRepositoryImpl.kt
@@ -0,0 +1,29 @@
+package ru.gb.popularlibrary
+
+import android.os.Looper
+import android.os.Handler
+
+
+private const val DATA_LOADINGFAKE_DELAY = 1000L
+
+class FakeUsersRepositoryImpl : UsersRepository {
+
+    private val data: List<UserEntity> = listOf(
+        UserEntity("mojombo", 1, "MDQ6VXNlcjE=","https://avatars.githubusercontent.com/u/1?v=4"),
+        UserEntity("defunkt", 2, "MDQ6VXNlcjI=","https://avatars.githubusercontent.com/u/2?v=4"),
+        UserEntity("pjhyett", 3, "MDQ6VXNlcjM=","https://avatars.githubusercontent.com/u/3?v=4"),
+        UserEntity("mojombo", 4, "MDQ6VXNlcjQ=","https://avatars.githubusercontent.com/u/4?v=4"),
+        UserEntity("mojombo", 5, "MDQ6VXNlcjE=","https://avatars.githubusercontent.com/u/1?v=4"),
+        UserEntity("defunkt", 6, "MDQ6VXNlcjI=","https://avatars.githubusercontent.com/u/2?v=4"),
+        UserEntity("pjhyett", 7, "MDQ6VXNlcjM=","https://avatars.githubusercontent.com/u/3?v=4"),
+        UserEntity("mojombo", 8, "MDQ6VXNlcjQ=","https://avatars.githubusercontent.com/u/4?v=4"),
+    )
+
+    override fun getUsers(onSuccess: (List<UserEntity>) -> Unit, onError: ((Throwable) -> Unit)?) {
+        Handler(Looper.getMainLooper()).postDelayed({
+            onSuccess(data)
+        },DATA_LOADINGFAKE_DELAY)
+    }
+}
+
+
--- app/src/main/java/ru/gb/popularlibrary/MainActivity.kt
+++ app/src/main/java/ru/gb/popularlibrary/MainActivity.kt
@@ -2,18 +2,23 @@ package ru.gb.popularlibrary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import ru.gb.popularlibrary.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private val adapter = UsersAdapter()
-    private val usersRepo:UsersRepo
+    private val usersRepository:UsersRepository = FakeUsersRepositoryImpl()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.reloadButton.setOnClickListener {
+            initRecyclerView()
+        }
 
         initRecyclerView()
     }
@@ -21,9 +26,12 @@ class MainActivity : AppCompatActivity() {
     private fun initRecyclerView() {
         binding.userReloadRecyclerview.layoutManager = LinearLayoutManager(this)
         binding.userReloadRecyclerview.adapter = adapter
-        usersRepo.getUsers {
-            adapter.setData()
-        }
+        usersRepository.getUsers (
+            onSuccess = adapter::setData,
+            onError = {
+                Toast.makeText(this, "Error load data", Toast.LENGTH_SHORT).show()
+            }
+        )
 
     }
 }
