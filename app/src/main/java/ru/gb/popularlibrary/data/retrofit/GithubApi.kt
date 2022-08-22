ee706874b5521b02c6c87ec3392c4fe8a5951ca7
vikzak <git@azbukait.ru>
2022-08-22 23:48:44 +0300

    Запуск проекта на RxJava3

--- app/src/main/java/ru/gb/popularlibrary/data/retrofit/GithubApi.kt
+++ app/src/main/java/ru/gb/popularlibrary/data/retrofit/GithubApi.kt
@@ -1,13 +1,21 @@
 package ru.gb.popularlibrary.data.retrofit
 
+
+
+import io.reactivex.rxjava3.core.Observable
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
 import ru.gb.popularlibrary.domain.entities.UserEntity
 
+// было до реактивщины
+//interface GithubApi {
+//    //@GET("users/{user}/repos")
+//    @GET("users")
+//    fun getUsers(): Call<List<UserEntity>>
+//}
 
 interface GithubApi {
-    //@GET("users/{user}/repos")
     @GET("users")
-    fun getUsers(): Call<List<UserEntity>>
+    fun getUsers(): Observable<List<UserEntity>>
 }
--- app/src/main/java/ru/gb/popularlibrary/data/retrofit/RetrofitUsersRepositoryImpl.kt
+++ app/src/main/java/ru/gb/popularlibrary/data/retrofit/RetrofitUsersRepositoryImpl.kt
@@ -6,6 +6,8 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
+
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
 import ru.gb.popularlibrary.domain.entities.UserEntity
@@ -18,9 +20,10 @@ class RetrofitUsersRepositoryImpl : UsersRepository {
 
     // использование Gson
     private val retrofit: Retrofit? = Retrofit.Builder()
-    .baseUrl("https://api.github.com/")
-    .addConverterFactory(GsonConverterFactory.create())
-    .build()
+        .baseUrl("https://api.github.com/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+        .build()
 
     // использование Moshi
 //    private val retrofit: Retrofit? = Retrofit.Builder()
@@ -31,26 +34,29 @@ class RetrofitUsersRepositoryImpl : UsersRepository {
     private val apiGithub: GithubApi = retrofit!!.create(GithubApi::class.java)
 
     override fun getUsers(onSuccess: (List<UserEntity>) -> Unit, onError: ((Throwable) -> Unit)?) {
-        apiGithub.getUsers().enqueue(object : Callback<List<UserEntity>>{
-            override fun onResponse(
-                call: Call<List<UserEntity>>,
-                response: Response<List<UserEntity>>
-            ) {
-                val body = response.body()
-                if (response.isSuccessful && body!=null){
-                    onSuccess.invoke(body)
-                } else {
-                    if (onError != null) {
-                        onError.invoke(IllegalStateException("No data"))
-                    }
-                }
-            }
-
-            override fun onFailure(call: Call<List<UserEntity>>, t: Throwable) {
-                onError?.invoke(IllegalStateException("No data"))
-            }
-
-        })
+//        apiGithub.getUsers().enqueue(object : Callback<List<UserEntity>> {
+//            override fun onResponse(
+//                call: Call<List<UserEntity>>,
+//                response: Response<List<UserEntity>>
+//            ) {
+//                val body = response.body()
+//                if (response.isSuccessful && body != null) {
+//                    onSuccess.invoke(body)
+//                } else {
+//                    if (onError != null) {
+//                        onError.invoke(IllegalStateException("No data"))
+//                    }
+//                }
+//            }
+//
+//            override fun onFailure(call: Call<List<UserEntity>>, t: Throwable) {
+//                onError?.invoke(IllegalStateException("No data"))
+//            }
+//
+//        })
+        apiGithub.getUsers().subscribe {
+            onSuccess.invoke(it)
+        }
     }
 }
 
