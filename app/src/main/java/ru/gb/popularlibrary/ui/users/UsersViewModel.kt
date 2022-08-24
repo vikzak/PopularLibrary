943929df6602753a5ce4b34b1ef458620d372209
vikzak <git@azbukait.ru>
2022-08-24 19:40:26 +0300

    Использование подписки и отписки с Disposable

--- app/src/main/java/ru/gb/popularlibrary/ui/users/UsersViewModel.kt
+++ app/src/main/java/ru/gb/popularlibrary/ui/users/UsersViewModel.kt
@@ -2,6 +2,11 @@ package ru.gb.popularlibrary.ui.users
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.kotlin.subscribeBy
+import io.reactivex.rxjava3.subjects.BehaviorSubject
+import io.reactivex.rxjava3.subjects.Subject
 import ru.gb.popularlibrary.domain.entities.UserEntity
 import ru.gb.popularlibrary.domain.repositories.UsersRepository
 import ru.gb.popularlibrary.utils.SingleEventsLiveData
@@ -16,34 +21,40 @@ class UsersViewModel(
 //    override val usersLiveData: LiveData<List<UserEntity>>
 //        get() = _usersLiveData
 // стало:
-    override val usersLiveData: LiveData<List<UserEntity>> = MutableLiveData()
-    override val errorLiveData: LiveData<Throwable> = SingleEventsLiveData()
-    override val progressLiveData: LiveData<Boolean> = MutableLiveData()
-    override val userDetailLiveData: LiveData<Unit> = SingleEventsLiveData()
+    override val usersLiveData: Observable<List<UserEntity>> = BehaviorSubject.create()
+    override val errorLiveData: Observable<Throwable> = BehaviorSubject.create()
+    override val progressLiveData: Observable<Boolean> = BehaviorSubject.create()
+    override val userDetailLiveData: Observable<Unit> = BehaviorSubject.create()
 
     override fun onRefresh() {
         loadData()
     }
 
     override fun onUserClick(userEntity: UserEntity) {
-        userDetailLiveData.mutable().postValue(Unit)
+        userDetailLiveData
     }
 
     private fun loadData() {
-        progressLiveData.mutable().postValue(true)
-        usersRepository.getUsers(
+        progressLiveData.mutable().onNext(true)
+
+        usersRepository.getUsers()
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeBy (
             onSuccess = {
-                progressLiveData.mutable().postValue(false)
-                usersLiveData.mutable().postValue(it)
+                progressLiveData.mutable().onNext(false)
+                usersLiveData.mutable().onNext(it)
             },
             onError = {
-                progressLiveData.mutable().postValue(false)
-                errorLiveData.mutable().postValue(it)
+                progressLiveData.mutable().onNext(false)
+                errorLiveData.mutable().onNext(it)
             }
         )
     }
 
     private fun <T> LiveData<T>.mutable():MutableLiveData<T>{
        return this as? MutableLiveData<T> ?: throw IllegalStateException("It is NOT MutableLiveData")
+    }
+    private fun <T> Observable<T>.mutable():Subject<T>{
+        return this as? Subject<T> ?: throw IllegalStateException("It is NOT MutableLiveData")
     }
 }
